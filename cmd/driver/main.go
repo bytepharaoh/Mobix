d7ee116801910452ef4154df6da2779fe7e6bca2
Hramov2 <hramovpi@gmail.com>
2026-06-03 12:56:39 +0300

    feat: Implemented Mongo connection and disconnection process

---
+++ cmd/driver/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/bytepharaoh/Mobix/internal/driver/db"
+	"github.com/bytepharaoh/Mobix/pkg/config"
+	"github.com/bytepharaoh/Mobix/pkg/logger"
+)
+
+func main() {
+
+	log := logger.New()
+	port := config.GetString("DRIVER_PORT", "8083")
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"ok","service":"gateway"}`))
+	})
+	srv := &http.Server{
+		Addr:         ":" + port,
+		Handler:      mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	// Mongo Connection
+	client, err := db.ConnectMongo()
+	if err != nil {
+		log.Error("Connection to Mongo failed", err)
+		os.Exit(1)
+	}
+	log.Info("Connected to Mongo succesfully")
+
+
+	go func() {
+		log.Info("driver service starting", slog.String("port", port))
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Error("server error", slog.String("error", err.Error()))
+			os.Exit(1)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Info("shutting down gracefully...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	// Disconnect mongo on shutdown
+	if err := client.Disconnect(ctx); err != nil {
+		log.Error("Failed to disconnect from Mongo", err.Error())
+	} else {
+		log.Info("Successfully disconnected from Mongo")
+	}
+	srv.Shutdown(ctx)
+}
---
+++ internal/driver/db/mongo.go
@@ -0,0 +1,58 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/bytepharaoh/Mobix/pkg/config"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func ConnectMongo() (*mongo.Client, error) {
+	// Getting .env variables
+	mongoUser := config.GetString("MONGO_USER", "admin")
+	mongoPwd := config.GetString("MONGO_PASSWORD", "password")
+	//mongoHost := config.GetString("MONGO_HOST", "mongodb")
+	// For development process
+	mongoHost := config.GetString("MONGO_HOST_OUTSIDE_DOCKER", "localhost")
+	mongoPort := config.GetString("MONGO_PORT", "27017")
+
+	// Constructing Mongo connection uri
+	mongoUri := fmt.Sprintf(
+		"mongodb://%s:%s@%s:%s",
+		mongoUser,
+		mongoPwd,
+		mongoHost,
+		mongoPort,
+	)
+
+	client, err := mongo.Connect(options.Client().ApplyURI(mongoUri))
+
+	if err != nil {
+		return nil, fmt.Errorf("mongo connection. %w", err)
+	}
+
+	// Creating a context to ping mongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// To cancel context in any case
+	defer cancel()
+
+	// Ping mongoDB
+	if err := client.Ping(ctx, nil); err != nil {
+		// Disconnect and return error if smth goes wrong
+		_ = client.Disconnect(context.Background())
+		return nil, fmt.Errorf("ping mongo. %w", err)
+	}
+
+	return client, nil
+}
+
+func DisconnectMongo(client *mongo.Client) error {
+	err := client.Disconnect(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to disconnect mongo. %w", err)
+	}
+	return nil
+}
