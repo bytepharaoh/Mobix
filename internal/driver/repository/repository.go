d7ed339e1a77a9b237ae199f6afc3d87d93479c1
Hramov2 <hramovpi@gmail.com>
2026-06-25 13:45:56 +0300

    fix(repo): formatted comments in a repository.go

--- internal/driver/repository/repository.go
+++ internal/driver/repository/repository.go
@@ -9,10 +9,11 @@ import (
 
 // Repository defines what the trip service needs from the database.
 // The service layer depends on this interface, not on MongoDB directly.
+// IncreaseTotalTrips designed for portential batch processing
+// (for example refresh amoutn of trips for drivers once per day)
 type Repository interface {
 	Create(ctx context.Context, trip *domain.Driver) (*domain.Driver, error)
 	FindById(ctx context.Context, id string) (*domain.Driver, error)
-	// For portential batch processing (for example refresh amoutn of trips for drivers once per day)
 	IncreaseTotalTrips(ctx context.Context, id string, tripDelta int) error
 	UpdateStatus(ctx context.Context, id string, status domain.DriverStatus) error
 	UpdateLastLogin(ctx context.Context, id string, time time.Time) error
