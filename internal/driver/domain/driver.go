b7202f39fd497fe40843a1ca6001ad078df8da33
Hramov2 <hramovpi@gmail.com>
2026-06-25 13:36:41 +0300

    feat(repo): Added repo methods for data manipulation, added tests. Follow interface style

--- internal/driver/domain/driver.go
+++ internal/driver/domain/driver.go
@@ -3,7 +3,7 @@ package domain
 import (
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 // Driver statuses
@@ -23,7 +23,7 @@ type Location struct {
 
 // Struct for a driver entity
 type Driver struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	ID          bson.ObjectID      `bson:"_id,omitempty" json:"id"`
 	Name        string             `bson:"name" json:"name"`
 	Email       string             `bson:"email" json:"email"`
 	Phone       string             `bson:"phone" json:"phone"`
---
+++ internal/driver/repository/mongo.go
@@ -0,0 +1,158 @@
+package repository
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	
+	"github.com/bytepharaoh/Mobix/internal/driver/domain"
+	apperrors "github.com/bytepharaoh/Mobix/pkg/errors"
+)
+
+type MongoRepository struct {
+	collection *mongo.Collection
+}
+
+func NewMongoRepository(database *mongo.Database) Repository {
+	return &MongoRepository {
+		collection: database.Collection("driver"),
+	}
+}
+
+func (r *MongoRepository) Create(ctx context.Context, driver *domain.Driver) (*domain.Driver, error) {
+	now := time.Now()
+	driver.UpdatedAt = now
+	driver.CreatedAt = now
+	result, err := r.collection.InsertOne(ctx, driver)
+	if err != nil {
+		return nil, apperrors.ErrInvalidInput
+	}
+	driver.ID = result.InsertedID.(bson.ObjectID)
+	return driver, nil
+}
+
+func (r *MongoRepository) FindById(ctx context.Context, id string) (*domain.Driver, error) {
+	objectID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, apperrors.ErrInvalidInput
+	}
+	var driver domain.Driver
+	err = r.collection.FindOne(ctx, bson.M{"_id": objectID}).Decode(&driver)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, domain.ErrDriverNotFound
+		}
+		return nil, apperrors.ErrInternal
+
+	}
+	return &driver, nil
+}
+
+func (r *MongoRepository) IncreaseTotalTrips(ctx context.Context, id string, tripDelta int,) error {
+	objectID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return apperrors.ErrInvalidInput
+	}
+
+	result, err := r.collection.UpdateOne(
+		ctx,
+		bson.M{"_id": objectID},
+		bson.M{
+			"$inc": bson.M{
+				"total_trips": tripDelta,
+			},
+			"$set": bson.M{
+				"updated_at": time.Now(),
+			},
+		},
+	)
+	if err != nil {
+		return apperrors.ErrInternal
+	}
+	if result.MatchedCount == 0 {
+		return domain.ErrDriverNotFound
+	}
+	
+
+	return nil
+}
+
+func (r *MongoRepository) UpdateStatus(ctx context.Context, id string, status domain.DriverStatus,) error {
+	objectID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return apperrors.ErrInvalidInput
+	}
+
+	result, err := r.collection.UpdateOne(
+		ctx,
+		bson.M{"_id": objectID},
+		bson.M{
+			"$set": bson.M{
+				"status":     status,
+				"updated_at": time.Now(),
+			},
+		},
+	)
+	if err != nil {
+		return apperrors.ErrInternal
+	}
+	if result.MatchedCount == 0 {
+		return domain.ErrDriverNotFound
+	}
+
+	return nil
+}
+
+func (r *MongoRepository) UpdateLastLogin(ctx context.Context, id string, loginTime time.Time,) error {
+	objectID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return apperrors.ErrInvalidInput
+	}
+
+	result, err := r.collection.UpdateOne(
+		ctx,
+		bson.M{"_id": objectID},
+		bson.M{
+			"$set": bson.M{
+				"last_login_at": loginTime,
+				"updated_at":    time.Now(),
+			},
+		},
+	)
+	if err != nil {
+		return apperrors.ErrInternal
+	}
+	if result.MatchedCount == 0 {
+		return domain.ErrDriverNotFound
+	}
+
+	return nil
+}
+
+func (r *MongoRepository) UpdateRating(ctx context.Context, id string, rating float64,) error {
+	objectID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return apperrors.ErrInvalidInput
+	}
+
+	result, err := r.collection.UpdateOne(
+		ctx,
+		bson.M{"_id": objectID},
+		bson.M{
+			"$set": bson.M{
+				"rating":     rating,
+				"updated_at": time.Now(),
+			},
+		},
+	)
+	if err != nil {
+		return apperrors.ErrInternal
+	}
+	if result.MatchedCount == 0 {
+		return domain.ErrDriverNotFound
+	}
+
+	return nil
+}
